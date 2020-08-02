b2119bd59de6d559b3a8848366d9313018fa166c
Vladimir Avstirychenko <liquid-05@mail.ru>
2020-08-02 10:17:25 +0300

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Владимир on 02.08.2020.
+//  Copyright © 2020 VladCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
